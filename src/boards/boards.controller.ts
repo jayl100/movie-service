68ed8544fa010a3a1bad3e7571ce0598288164d7
Jay.Hyeon <hyinky@gmail.com>
2025-02-24 17:19:52 +0900

    feat: pipes

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,19 +1,66 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Put, UsePipes, ValidationPipe,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './boards.model';
+import { Board, BoardStatus } from './boards.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get('/')
+  @Get()
   getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board | undefined {
+    if (id === undefined) {
+      throw new NotFoundException('Not Found Page');
+    }
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+
+  @Put('/:id')
+  updateBoard(
+    @Param('id') id: string,
+    @Body() updateBoardDto: UpdateBoardDto,
+  ): Board | undefined {
+    if (id === undefined) {
+      throw new NotFoundException('Not Found Page');
+    }
+    return this.boardsService.updateBoard(id, updateBoardDto);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ): Board | undefined {
+    if (id === undefined) {
+      throw new NotFoundException('Not Found Page');
+    }
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
+
   // 다른 컴포넌트에서 boards를 수정하지 않기 위해 private를 사용
 
   getAllBoards(): Board[] {
@@ -23,5 +25,37 @@ export class BoardsService {
 
     this.boards.push(board);
     return board;
+  }
+
+  getBoardById(id: string): Board | undefined {
+    const foundBoard = this.boards.find((board) => board.id === id);
+    if (!foundBoard) {
+      throw new NotFoundException(`Not Found ${id} Page`);
+    }
+    return foundBoard;
+  }
+
+  deleteBoard(id: string): void {
+    const foundBoard = this.getBoardById(id);
+    if (foundBoard) {
+      this.boards = this.boards.filter((board) => board.id !== foundBoard.id);
+    }
+  }
+
+  updateBoard(id: string, updateBoardDto: UpdateBoardDto): Board | undefined {
+    const foundBoard = this.getBoardById(id);
+    if (foundBoard) {
+      foundBoard.title = updateBoardDto.title;
+      foundBoard.description = updateBoardDto.description;
+    }
+    return foundBoard;
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board | undefined {
+    const foundBoard = this.getBoardById(id);
+    if (foundBoard) {
+      foundBoard.status = status;
+    }
+    return foundBoard;
   }
 }
